33ddcc4ccc2b8e2d40a70111f17af5a26e220919
Pol Vallverdu <86187892+polvallverdu@users.noreply.github.com>
2025-06-18 18:34:17 +0200

    Add logout loading feedback and API param object

--- src/lib/server/orpc/context.ts
+++ src/lib/server/orpc/context.ts
@@ -1,8 +1,10 @@
 import { os } from "@orpc/server";
 import type { UserContext } from "$lib/server/api";
+import type { Cookies } from "@sveltejs/kit";
 
 export const osBase = os.$context<{
   headers: Headers;
   ctx?: ExecutionContext;
   userCtx: UserContext;
+  cookies?: Cookies;
 }>();
