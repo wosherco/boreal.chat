c119da48877dc0e6c093c2718658227e2a4eefdc
Pol Vallverdu <86187892+polvallverdu@users.noreply.github.com>
2025-06-18 18:12:09 +0200

    Use page store in error component

---
+++ src/routes/+error.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import { page } from "$app/state";
+  import { Button } from "$lib/components/ui/button";
+</script>
+
+<div class="m-auto flex h-full w-full flex-col items-center justify-center gap-4 p-4 text-center">
+  <h1 class="text-4xl font-bold">{page.status}</h1>
+  <p class="text-muted-foreground text-lg">{page.error?.message}</p>
+  <Button variant="link" href="/">Go back home -&gt;</Button>
+</div>
