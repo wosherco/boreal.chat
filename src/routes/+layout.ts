8ee05f0e89634f0a5f3b6853a27412441de0d5b4
polvallverdu <pvallverdualcaide@gmail.com>
2025-06-19 00:16:40 +0200

    fix: posthog

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,12 +1,13 @@
 import posthog from "posthog-js";
 import { browser } from "$app/environment";
-import { onMount } from "svelte";
 
-onMount(() => {
+export const load = async () => {
   if (browser) {
     posthog.init("phc_a2qpwc0fnaCPlkK4kBZe0mZrdQiFSBbdCE0pNtCnGpZ", {
       api_host: "https://eu.i.posthog.com",
       person_profiles: "identified_only",
     });
   }
-});
+
+  return;
+};
