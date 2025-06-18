fc2fda036d1dd43ec4d52867ff8284bb29ab5dc9
polvallverdu <pvallverdualcaide@gmail.com>
2025-06-18 19:43:04 +0200

    fix: wrong filter for chats

--- src/routes/(chat)/+layout.server.ts
+++ src/routes/(chat)/+layout.server.ts
@@ -3,32 +3,37 @@ import { SIDEBAR_COLLAPSED_COOKIE } from "$lib/common/cookies";
 import { setPossiblySPAData, type Chat, type ServerData } from "$lib/common/sharedTypes";
 import { db } from "$lib/server/db";
 import { chatTable } from "$lib/server/db/schema";
-import { desc } from "drizzle-orm";
+import { desc, eq } from "drizzle-orm";
 import { transformKeyToCamelCaseRecursive } from "$lib/client/hooks/utils";
 
-export const load: LayoutServerLoad = async ({ cookies, parent, params }) => {
+export const load: LayoutServerLoad = async ({ cookies, parent, params, locals }) => {
   const parentData = await parent();
 
+  const userId = locals.user?.id;
+
   const sidebarCollapsed = cookies.get(SIDEBAR_COLLAPSED_COOKIE) === "true";
-  const lastChats = db
-    .select({
-      id: chatTable.id,
-      title: chatTable.title,
-      createdAt: chatTable.createdAt,
-      updatedAt: chatTable.updatedAt,
-    })
-    .from(chatTable)
-    .orderBy(desc(chatTable.updatedAt))
-    .limit(40)
-    .execute()
-    .then((rows) =>
-      rows.map(
-        (row) =>
-          transformKeyToCamelCaseRecursive(
-            row as unknown as Record<string, unknown>,
-          ) as unknown as Chat,
-      ),
-    ) satisfies ServerData<Chat[]>;
+  const lastChats = userId
+    ? (db
+        .select({
+          id: chatTable.id,
+          title: chatTable.title,
+          createdAt: chatTable.createdAt,
+          updatedAt: chatTable.updatedAt,
+        })
+        .from(chatTable)
+        .where(eq(chatTable.userId, userId))
+        .orderBy(desc(chatTable.updatedAt))
+        .limit(40)
+        .execute()
+        .then((rows) =>
+          rows.map(
+            (row) =>
+              transformKeyToCamelCaseRecursive(
+                row as unknown as Record<string, unknown>,
+              ) as unknown as Chat,
+          ),
+        ) satisfies ServerData<Chat[]>)
+    : [];
 
   return {
     ...setPossiblySPAData(parentData),
