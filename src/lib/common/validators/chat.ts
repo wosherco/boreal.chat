8e5677d0ad1806bb9151ea605f0fb985242ed3ca
polvallverdu <pvallverdualcaide@gmail.com>
2025-07-05 12:20:34 +0200

    feat: better chat title validator

--- src/lib/common/validators/chat.ts
+++ src/lib/common/validators/chat.ts
@@ -1,3 +1,7 @@
 import { z } from "zod";
 
-export const chatTitleSchema = z.string().min(2).max(100);
+export const chatTitleSchema = z
+  .string()
+  .trim()
+  .min(2, { message: "Chat title must be at least 2 characters long" })
+  .max(100, { message: "Chat title must be at most 100 characters long" });
