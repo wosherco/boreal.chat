fcb43626a5ad7a91b3ff0453b6f3c3534782270a
polvallverdu <pvallverdualcaide@gmail.com>
2025-08-07 20:53:19 +0200

    fix: couple of things

--- src/lib/client/utils/runed.ts
+++
@@ -1,8 +0,0 @@
-import { browser } from "$app/environment";
-
-export type ConfigurableWindow = {
-  /** Provide a custom `window` object to use in place of the global `window` object. */
-  window?: typeof globalThis & Window;
-};
-
-export const defaultWindow = browser && typeof window !== "undefined" ? window : undefined;
